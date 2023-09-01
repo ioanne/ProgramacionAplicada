bdba4f948f847527174efbee0e37464dfa13360b
Juan Ignacio Bonini <juanignaciobonini@hotmail.com>
2023-08-31 21:49:41 -0300

    Se agrega comentario.

--- Ejemplos/Clase3/modularizar/implementacion.py
+++ Ejemplos/Clase3/modularizar/implementacion.py
@@ -5,6 +5,8 @@ from perro import Perro
 perro = Perro("dylan")
 gato = Gato("Turing")
 
+
+# Implementamos la logica del algoritmo
 print(f"El perro {perro.nombre} dice {perro.hacer_sonido()}")
 print(f"El gato {gato.nombre} dice {gato.hacer_sonido()}")
 
