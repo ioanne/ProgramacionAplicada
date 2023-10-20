4d461ea4d3e66c7d65aa098a9f1d51008e23330b
Juan Ignacio Bonini <juanignaciobonini@hotmail.com>
2023-10-19 21:15:17 -0300

    Clase 8.1

---
+++ ejemplos/clase8/main_class.py
@@ -0,0 +1,6 @@
+"""
+Hacer una nueva clase que utilice la API creada anteriormente
+y convierta los resultados de dicha API en objetos.
+
+Preferentemente utilizar la librería Pydantic e investigar como se utiliza.
+"""
