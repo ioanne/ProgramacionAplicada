4c5da30e173b5397024c02c0c9b051fe06f65adc
Juan Ignacio Bonini <juanignaciobonini@hotmail.com>
2023-10-19 21:06:12 -0300

    Subimos la clase 9

--- ejemplos/clase8/main.py
+++ ejemplos/clase8/main.py
@@ -1,4 +1,4 @@
-from api import RickAndMortyAPI
+from api import RickAndMortyAPI, ManyCharactersException
 from character import Character
 
 api = RickAndMortyAPI()
@@ -17,6 +17,36 @@ character = None  # Hace que el personaje sea None para evitar traer dato errone
 if character_data:
     character = Character(**character_data)
     print(character.name)
+
+episodes = api.get_episodes()
+episode_5 = api.get_episode(5)
+
+locations = api.get_locations()
+location_5 = api.get_location(5)
+
+characters = api.get_characters()
+
+
+characters_on_episode = api.get_characters_on_episode(5)
+
+rick_sanchez = None
+try:  # Intentar
+    # Intentamos obtener un personaje por un nombre parcial
+    # Si obtenemos solo 1 se completa la variable rick_sanchez.
+    rick_sanchez = api.get_character_by_name("Rick Sanchez")
+    print(f"Nombre: {rick_sanchez}")
+    rick_sanchez = api.get_character_by_name("Ri")
+except ManyCharactersException as error:
+    # Si obtenemos más o menos de 1 personaje da error.
+    # Capturamos el error y mostramos un mensaje informativo
+    print(f"{error}")
+else:
+    # Lo vemos despues
+    pass
+finally:
+    # Lo vemos despues
+    pass
+
 
 # API
 # Character
